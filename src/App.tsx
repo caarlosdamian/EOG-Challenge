bba10f89b8c333542df780d0f168dc8e0b9e4eaa
caarlosdamian <dapecarlos@gmail.com>
2021-09-19 15:31:11 -0500

    Feat/Add SelectMetric Component,State App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Wrapper from './components/Wrapper';
-import SelectMetric from './Features/SelectMetric/SelectMetric';
+import {SelectMetric} from './Features/SelectMetric/SelectMetric';
 
 const theme = createTheme({
   palette: {
@@ -21,15 +21,18 @@ const theme = createTheme({
   },
 });
 
-const App = () => (
-  <MuiThemeProvider theme={theme}>
-    <CssBaseline />
-    <Wrapper>
-      <Header />
-      <SelectMetric />
-      <ToastContainer />
-    </Wrapper>
-  </MuiThemeProvider>
-);
+const App = () => {
+  const [globalMetric, setGlobalMetric] = useState('');
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Wrapper>
+        <Header />
+        <SelectMetric setGlobalMetric={setGlobalMetric} globalMetric={globalMetric} />
+        <ToastContainer />
+      </Wrapper>
+    </MuiThemeProvider>
+  );
+};
 
 export default App;
--- src/Features/SelectMetric/SelectMetric.tsx
+++ src/Features/SelectMetric/SelectMetric.tsx
@@ -1,5 +1,65 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
+import {
+  createStyles,
+  Container,
+  FormControl,
+  InputLabel,
+  LinearProgress,
+  makeStyles,
+  MenuItem,
+  Select,
+  Theme,
+  Typography,
+} from '@material-ui/core';
 
-export const SelectMetric = () => <div>Hello</div>;
-
-export default SelectMetric;
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+  }),
+);
+const query = gql`
+  query {
+    getMetrics
+  }
+`;
+interface Props {
+  setGlobalMetric: (val: string) => void;
+  globalMetric: string;
+}
+export const SelectMetric: React.FC<Props> = ({ setGlobalMetric, globalMetric }) => {
+  const classes = useStyles();
+  const { loading, error, data } = useQuery(query);
+  if (loading) {
+    return <LinearProgress />;
+  }
+  if (error) {
+    <Typography>{error}</Typography>;
+  }
+  const handlechange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setGlobalMetric(event.target.value as string);
+  };
+  return (
+    <Container>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">Metric</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={globalMetric}
+          label="Metric"
+          onChange={handlechange}
+        >
+          {data.getMetrics.map((metric: string) => (
+            <MenuItem key={metric} value={metric}>
+              {metric}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Container>
+  );
+};
