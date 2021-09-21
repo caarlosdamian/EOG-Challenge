ae5db8048ed34cdaf966df31a0546f5a41a7586e
caarlosdamian <dapecarlos@gmail.com>
2021-09-21 12:56:02 -0500

    Feat/Add webSocket config

--- src/index.js
+++ src/index.js
@@ -4,10 +4,33 @@ import ReactDOM from 'react-dom';
 import { Store } from './redux/store';
 import { Provider } from 'react-redux';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { split, HttpLink } from '@apollo/client';
 
 import App from './App';
-const client = new ApolloClient({
+import { getMainDefinition } from '@apollo/client/utilities';
+
+const httpLink = new HttpLink({
   uri: 'https://react.eogresources.com/graphql',
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'wss://react.eogresources.com/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
